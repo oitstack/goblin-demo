21f194c373d8746ecea9f2f8c4281edded5e4e6f
yangguang-git002 <yangguang1@oppo.com>
2022-06-28 18:18:01 +0800

    init-commit

---
+++ src/main/java/goblin/demo/entity/TestTable.java
@@ -0,0 +1,15 @@
+package goblin.demo.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+
+
+@Data
+@ToString
+public class TestTable {
+
+    private Long id;
+
+    private String business;
+}
